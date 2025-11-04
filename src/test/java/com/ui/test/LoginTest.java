07d3415abc6f74e1d7cc546ea798172eb9b1b7b4
daraju <daraju@EAV1067779.eac.ad.ea.com>
2025-11-03 22:46:20 -0800

    Add the neagtive scenario into LoginTest

--- src/test/java/com/ui/test/LoginTest.java
+++ src/test/java/com/ui/test/LoginTest.java
@@ -18,20 +18,14 @@ import com.utility.LoggerUtility;
 @Listeners(com.ui.listners.TestListner.class)
 public class LoginTest extends TestBase {
 
-	/*
-	 * @Test(description =
-	 * "Verify if the valid user is able to login into the application", groups = {
-	 * "e2e", "smoke" }, dataProviderClass =
-	 * com.ui.dataproviders.LoginDataProvider.class, dataProvider =
-	 * "LoginTestCSVDataProvider", retryAnalyzer =
-	 * com.ui.listners.MyRetryAnalyzer.class) public void loginTestWithCSV(User
-	 * user) {
-	 * 
-	 * assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(),
-	 * user.getPassword()).getUserName(), "Dav Raj");
-	 * 
-	 * }
-	 */
+	@Test(description = "Verify if the valid user is able to login into the application", groups = { "e2e",
+			"smoke" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider", retryAnalyzer = com.ui.listners.MyRetryAnalyzer.class)
+	public void loginTestWithCSV(User user) {
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Dav Raj");
+
+	}
 
 	@Test(description = "Verify if the valid user is able to login into the application", groups = { "e2e",
 			"smoke" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listners.MyRetryAnalyzer.class)
